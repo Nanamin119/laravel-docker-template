2ad38bcf37b0484a0fc30e06ef40a8235a37523b
林奈々実 <hayashinanami@MacBook-Air-2.local>
2025-11-23 02:07:16 +0900

    編集画面の表示

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -43,4 +43,12 @@ class TodoController extends Controller
         $todo = $this->todo->find($id);
         return view('todo.show', ['todo' => $todo]);
     }
+    // TODO: ルートパラメータを引数に受け取る
+    public function edit($id)
+    {
+        // TODO: 編集対象のレコードの情報を持つTodoモデルのインスタンスを取得
+        $todo = $this->todo->find($id);
+        // TODO: view()を使用して編集画面を表示
+        return view('todo.edit', ['todo' => $todo]);
+    }
 };
